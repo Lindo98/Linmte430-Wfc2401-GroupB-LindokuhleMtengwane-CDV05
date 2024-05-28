976f8f6c328df28771c8b737686dc2aed8dc9a76
Lindokuhle Mtengwane <lindokuhlemtengwane@gmail.com>
2024-05-28 14:11:51 +0200

    renamed the file

--- Portfolio piece/main.js
+++
@@ -1,8 +0,0 @@
-var typed= new Typed(".text", {
-    strings:["Software Developer", "Front-end Developer", "UI/UX Designer." ],
-    typeSpeed:150,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true,
-});
-
---
+++ Portfolio/main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings:["Software Developer", "Front-end Developer", "UI/UX Designer." ],
+    typeSpeed:150,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+});
+
